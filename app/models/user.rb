cfc82fcbfc703615f3913c5d60c73cd8af3f94a4
Vincent Tsang <vstsang@gmail.com>
2017-08-04 20:11:00 +0800

    Clean up custom mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ActiveRecord::Base
-#  after_create :send_registration_emails
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-#  private
-
-#  def send_registration_emails
-#    ConfirmMailer.confirmation_email(self).deliver_now
-#  end
 end
