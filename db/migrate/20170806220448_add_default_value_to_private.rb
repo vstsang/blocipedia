a85263b9c90b76ecbfedbe0eea04ae50c8015504
Vincent Tsang <vstsang@gmail.com>
2017-08-08 21:08:01 +0800

    Remove default migration for private attribute

--- db/migrate/20170806220448_add_default_value_to_private.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToPrivate < ActiveRecord::Migration
-  def change
-    change_column_default(:wikis, :private, from: nil, to: false)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 20170808110332) do
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "private",    default: false
+    t.boolean  "private"
     t.integer  "user_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
